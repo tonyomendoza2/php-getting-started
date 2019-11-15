e60e6807c6248b23893ee0439f1b8c8e97b62728
tonyomendoza2 <57787159+tonyomendoza2@users.noreply.github.com>
2019-11-15 00:57:04 -0800

    Update insertName.php

--- web/views/insertName.php
+++ web/views/insertName.php
@@ -1,6 +1,10 @@
 <?php
 
   $name = $_GET["name"];
+$statement = $link->prepare('INSERT INTO test_table (id, name)
+    VALUES (?, ?)');
+
+$statement->execute([1, $name]);
   echo "Test".$name;
 
 ?>
